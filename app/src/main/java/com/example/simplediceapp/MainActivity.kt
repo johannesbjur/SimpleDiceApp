de6b7aa5583e667905411de753eacc6e9b79f16f
Johannes Bjurströmer <55398759+johannesbjur@users.noreply.github.com>
2020-04-02 14:21:20 +0200

    adding images started

--- app/src/main/java/com/example/simplediceapp/MainActivity.kt
+++ app/src/main/java/com/example/simplediceapp/MainActivity.kt
@@ -4,19 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     var dice = Dice()
     lateinit var diceValueField: TextView
+    lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Log.d("MainActivity", dice.toString())
-        diceValueField = findViewById<TextView>( R.id.showNrField )
+        diceValueField  = findViewById( R.id.showNrField )
+        diceImage       = findViewById( R.id.diceImage )
 
     }
 
@@ -26,6 +29,8 @@ class MainActivity : AppCompatActivity() {
         Log.d("MainActivity", dice.toString())
 
         diceValueField.text = dice.toString()
+
+        diceImage.setImageResource(R.drawable.ic_dice_2)
 
     }
 
