61f8d3880f6b81d6d26cc8067b633e2216563d87
Johannes Bjurströmer <55398759+johannesbjur@users.noreply.github.com>
2020-04-02 14:41:20 +0200

    adding images finished

--- app/src/main/java/com/example/simplediceapp/MainActivity.kt
+++ app/src/main/java/com/example/simplediceapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.simplediceapp
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,7 +11,6 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
     var dice = Dice()
-    lateinit var diceValueField: TextView
     lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +18,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.d("MainActivity", dice.toString())
-        diceValueField  = findViewById( R.id.showNrField )
         diceImage       = findViewById( R.id.diceImage )
 
     }
@@ -26,11 +25,23 @@ class MainActivity : AppCompatActivity() {
     fun rollDiceClick( view: View) {
 
         dice.roll()
+
+        var iconName = R.drawable.ic_dice_1
+
+       when( dice.toString() ) {
+           "1" -> iconName = R.drawable.ic_dice_1
+           "2" -> iconName = R.drawable.ic_dice_2
+           "3" -> iconName = R.drawable.ic_dice_3
+           "4" -> iconName = R.drawable.ic_dice_4
+           "5" -> iconName = R.drawable.ic_dice_5
+           "6" -> iconName = R.drawable.ic_dice_6
+       }
+
+
         Log.d("MainActivity", dice.toString())
 
-        diceValueField.text = dice.toString()
 
-        diceImage.setImageResource(R.drawable.ic_dice_2)
+        diceImage.setImageResource(iconName)
 
     }
 
